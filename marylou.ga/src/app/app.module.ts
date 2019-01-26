549340072829c3efa9648605f91ab857f3a9746c
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-01-26 13:09:10 -0700

    Generate main modules and set up basic routing
    - Stub out the major modules (login, lobby, game) and add empty components.
    - Configure basic routing

--- marylou.ga/src/app/app.module.ts
+++ marylou.ga/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginModule } from './login/login.module';
+import { LobbyModule } from './lobby/lobby.module';
+import { GameModule } from './game/game.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    LobbyModule,
+    GameModule
   ],
   providers: [],
   bootstrap: [AppComponent]
