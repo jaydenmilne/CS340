e8406b94d727aaa113fba0db6ad95d2448fbc922
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-02-02 11:48:16 -0700

    Finish login page, add user service - Incoming client commands are now verified - Server proxy will now save the authToken (currently it doesn't use it) - Add user service to hold on to the user info - User model can be instantiated from a user-like object
    - Fix linting errors

--- client/src/app/login/login.service.ts
+++ client/src/app/login/login.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ServerProxyService } from '@core/server-proxy.service';
-import { LoginCommand, RegisterCommand, LoginResult } from '@core/logincommands';
+import { LoginCommand, RegisterCommand, LoginResult } from '@core/login-commands';
 import { CommandRouterService } from '@core/command-router.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root' // this makes the service a singleton
@@ -10,26 +11,43 @@ import { CommandRouterService } from '@core/command-router.service';
 export class LoginService {
 
   constructor(private serverProxy: ServerProxyService,
-              private commandRouter: CommandRouterService ) {
+              private commandRouter: CommandRouterService,
+              private router: Router ) {
     this.commandRouter.loginResult$.subscribe(
       result => this.handleResult(result)
     );
   }
 
-  private loginErrorSource = new Subject<string>();
-  public loginError$ = this.loginErrorSource.asObservable();
+  public loginError$ = new Subject<string>();
 
+  /**
+   * Parses a loginResult command and either routes away from login page
+   * or displays a login error.
+   * @param loginResult Client command to parse
+   */
   handleResult(loginResult: LoginResult) {
-    if (loginResult.authToken !== '') {
-      return; // wasn't a failure
+    if (loginResult.user !== null) {
+      // Login/Register worked
+      this.router.navigate(['/lobby']);
+      return;
     }
 
     // Signal the component
-    this.loginErrorSource.next(loginResult.error);
+    this.loginError$.next(loginResult.error);
   }
 
+  /**
+   * Begins the login process
+   */
   public commenceLogin(username: string, password: string) {
     this.serverProxy.transmitCommand(new LoginCommand(username, password));
+  }
+
+  /**
+   * Begins the registration process
+   */
+  public commenceRegister(username: string, password: string) {
+    this.serverProxy.transmitCommand(new RegisterCommand(username, password));
   }
 
 }
