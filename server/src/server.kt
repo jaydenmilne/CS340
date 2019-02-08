5a14b5efe7ddc1dc2c108f1aa9adbb1057e18ad1
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-02-08 12:48:06 -0700

    Fix server build and startup
    - Resolve build errors
    - Start the server
    - Logging
    - Send 500 error on unhandled exception

--- server/src/server.kt
+++ server/src/server.kt
@@ -3,8 +3,6 @@ import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpServer
 import commands.*
 import models.AuthTokens
-import org.apache.commons.io.IOUtils
-import sun.misc.IOUtils
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection.*
@@ -15,8 +13,10 @@ const val REGISTRATION_ENDPOINT = "/register"
 const val COMMAND_ENDPOINT = "/command"
 
 fun main(args: Array<String>) {
-    var server = HttpServer.create(InetSocketAddress(8080), MAX_CONNECTIONS)
+    val port = 4300
+    var server = HttpServer.create(InetSocketAddress(port), MAX_CONNECTIONS)
 
+    println("Registering endpoints...")
     server.createContext(COMMAND_ENDPOINT) { httpExchange ->
         when (httpExchange.requestMethod) {
             "GET" -> handleGet(httpExchange)
@@ -29,85 +29,117 @@ fun main(args: Array<String>) {
             "POST" -> handleRegistrationPost(httpExchange)
         }
     }
+    println("Hold on to your butts...")
+    server.start()
+    println("Server started on port " + port)
 }
 
 fun handleRegistrationPost(httpExchange: HttpExchange) {
+    httpExchange.responseHeaders.add("Access-Control-Allow-Origin", "*");
     var requestBody = org.apache.commons.io.IOUtils.toString(InputStreamReader(httpExchange.requestBody))
 
-    var initialCommand = Gson().fromJson(requestBody, IRegisterServerCommand::class.java)
+    println("POST /register")
+    println(requestBody)
 
-    var command = when(initialCommand.type) {
-        LOGIN -> Gson().fromJson(requestBody, LoginCommand::class.java)
-        REGISTER -> Gson().fromJson(requestBody, RegisterCommand::class.java)
-        else -> null
+    try {
+        var initialCommand = Gson().fromJson(requestBody, IRegisterServerCommand::class.java)
+
+        var command = when(initialCommand.type) {
+            LOGIN -> Gson().fromJson(requestBody, LoginCommand::class.java)
+            REGISTER -> Gson().fromJson(requestBody, RegisterCommand::class.java)
+            else -> null
+        }
+
+        if (command == null) {
+            httpExchange.sendResponseHeaders(HTTP_BAD_REQUEST, 0)
+        } else {
+            httpExchange.sendResponseHeaders(HTTP_OK, 0)
+            var writer = OutputStreamWriter(httpExchange.responseBody)
+
+            var resultCommand = command.execute()
+
+            writer.write(Gson().toJson(resultCommand, IRegisterClientCommand::class.java))
+        }
+    } catch (e : Exception) {
+        httpExchange.sendResponseHeaders(HTTP_INTERNAL_ERROR, 0)
     }
 
-    if (command == null) {
-        httpExchange.sendResponseHeaders(HTTP_BAD_REQUEST, 0)
-        httpExchange.close()
-    } else {
-        httpExchange.sendResponseHeaders(HTTP_OK, 0)
-        var writer = OutputStreamWriter(httpExchange.responseBody)
+    println(httpExchange.responseCode.toString())
+    httpExchange.close()
 
-        var resultCommand = command.execute()
-
-        writer.write(Gson().toJson(resultCommand, IRegisterClientCommand::class.java))
-        httpExchange.close()
-    }
 }
 
 fun handleGet(httpExchange: HttpExchange) {
-    var authToken = httpExchange.requestHeaders.getFirst("Authorization")
-    var user = AuthTokens.getUser(authToken)
+    httpExchange.responseHeaders.add("Access-Control-Allow-Origin", "*");
+    try {
+        var authToken = httpExchange.requestHeaders.getFirst("Authorization")
+        var user = AuthTokens.getUser(authToken)
 
-    httpExchange.sendResponseHeaders(HTTP_OK, 0)
+        println("GET /command - " + authToken)
 
-    if (user == null) {
-        httpExchange.sendResponseHeaders(HTTP_FORBIDDEN, 0)
-        httpExchange.close()
-        return
+
+        if (user == null) {
+            httpExchange.sendResponseHeaders(HTTP_FORBIDDEN, 0)
+            return
+        } else {
+            httpExchange.sendResponseHeaders(HTTP_OK, 0)
+            var writer = OutputStreamWriter(httpExchange.responseBody)
+            writer.write(user.queue.pollCommands())
+        }
+    } catch (e : Exception) {
+        httpExchange.sendResponseHeaders(HTTP_INTERNAL_ERROR, 0)
     }
 
-    var writer = OutputStreamWriter(httpExchange.responseBody)
 
-    writer.write(user.queue.pollCommands())
+    println(httpExchange.responseCode.toString())
     httpExchange.close()
 }
 
 fun handlePost(httpExchange: HttpExchange) {
+    // Handle CORS
+    httpExchange.responseHeaders.add("Access-Control-Allow-Origin", "*");
     var requestBody = org.apache.commons.io.IOUtils.toString(InputStreamReader(httpExchange.requestBody))
 
-    var initialCommand = Gson().fromJson(requestBody, INormalServerCommand::class.java)
+    try {
+        var initialCommand = Gson().fromJson(requestBody, INormalServerCommand::class.java)
 
-    var authToken = httpExchange.requestHeaders.getFirst("Authorization")
-    var user = AuthTokens.getUser(authToken)
+        var authToken = httpExchange.requestHeaders.getFirst("Authorization")
+        var user = AuthTokens.getUser(authToken)
 
-    if (user == null) {
-        httpExchange.sendResponseHeaders(HTTP_FORBIDDEN, 0)
-        httpExchange.close()
-        return
+
+        if (user == null) {
+            httpExchange.sendResponseHeaders(HTTP_FORBIDDEN, 0)
+            httpExchange.close()
+            return
+        }
+        println("POST /command - " + user.username)
+        println(requestBody)
+
+        var command = when (initialCommand.type) {
+            CREATE_GAME -> Gson().fromJson(requestBody, CreateGameCommand::class.java)
+            JOIN_GAME -> Gson().fromJson(requestBody, JoinGameCommand::class.java)
+            LEAVE_GAME -> Gson().fromJson(requestBody, LeaveGameCommand::class.java)
+            LIST_GAMES -> Gson().fromJson(requestBody, ListGamesCommand::class.java)
+            PLAYER_READY -> Gson().fromJson(requestBody, PlayerReadyCommand::class.java)
+            else -> null
+        }
+
+        if (command == null) {
+            httpExchange.sendResponseHeaders(HTTP_BAD_REQUEST, 0)
+        } else {
+            httpExchange.sendResponseHeaders(HTTP_OK, 0)
+            var writer = OutputStreamWriter(httpExchange.responseBody)
+
+            command.execute(user)
+
+            writer.write(user.queue.pollCommands())
+        }
+
+    } catch (e : Exception) {
+        httpExchange.sendResponseHeaders(HTTP_INTERNAL_ERROR, 0)
     }
 
-    var command = when(initialCommand.type) {
-        CREATE_GAME -> Gson().fromJson(requestBody, CreateGameCommand::class.java)
-        JOIN_GAME -> Gson().fromJson(requestBody, JoinGameCommand::class.java)
-        LEAVE_GAME -> Gson().fromJson(requestBody, LeaveGameCommand::class.java)
-        LIST_GAMES -> Gson().fromJson(requestBody, ListGamesCommand::class.java)
-        PLAYER_READY -> Gson().fromJson(requestBody, PlayerReadyCommand::class.java)
-        START_GAME -> Gson().fromJson(requestBody, StartGameCommand::class.java)
-        else -> null
-    }
+    println(httpExchange.responseCode.toString())
+    httpExchange.close()
 
-    if (command == null) {
-        httpExchange.sendResponseHeaders(HTTP_BAD_REQUEST, 0)
-        httpExchange.close()
-    } else {
-        httpExchange.sendResponseHeaders(HTTP_OK, 0)
-        var writer = OutputStreamWriter(httpExchange.responseBody)
-
-        command.execute(user)
-
-        writer.write(user.queue.pollCommands())
-        httpExchange.close()
-    }
 }
