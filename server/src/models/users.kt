d74e2c092e6ce3313f8354e6820bdc1d9ee40d58
FramesTheName <frame.mlf@gmail.com>
2019-02-09 22:43:21 -0700

    Colors added

--- server/src/models/users.kt
+++ server/src/models/users.kt
@@ -41,11 +41,12 @@ object Users {
 class User(var username: String) {
 
     var userID = getNextUserID()
-    private var passwordHash = ""
     var ready = false
     var authToken = ""
     var color = Color.YELLOW
 
+    @Transient
+    private var passwordHash = ""
     @Transient
     var queue = CommandQueue()
 
@@ -62,7 +63,7 @@ class User(var username: String) {
     }
 }
 
-// Simplification of user class to sendt to client
+// Simplification of user class to send to client
 class ClientUser(val userId: Int, var username: String, val authToken: String) {
     constructor() : this(0, "", "")
 }
