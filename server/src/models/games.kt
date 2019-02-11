371777ec6ab7527b277fe92f5076f0a2f0f7dc6a
Brennon Brimhall <brennonbrimhall@gmail.com>
2019-02-10 17:19:12 -0700

    Start game if everyone is ready.
    And make styling consistent.

--- server/src/models/games.kt
+++ server/src/models/games.kt
@@ -16,16 +16,12 @@ object Games {
      * Removes the user from any pending games. Throws a CommandException if they are in a game that has already
      * started.
      */
-    fun removeUserFromGames(user : User) {
-        // Check to see if the user is already in a game
-        for (id_game in Games.games) {
-            val game = id_game.value
+    fun removeUserFromGames(user: User) {
+        for (game in Games.games.values) {
             if (user in game.players) {
-                // If the user is already in a started game, we have issues
                 if (game.started) {
                     throw CommandException("JoinGameCommand: User is in a started game but tried to join another!")
                 } else {
-                    // The user is in a game that hasn't started yet - remove the player from that game
                     game.players.remove(user)
                 }
             }
--- server/src/models/users.kt
+++ server/src/models/users.kt
@@ -2,25 +2,25 @@ package models
 
 private var nextUserId = -1
 
-fun getNextUserID(): Int {
+fun getNextUserId(): Int {
     nextUserId++
     return nextUserId
 }
 
 object Users {
-    private var usersByUserID = mutableMapOf<Int, User>()
+    private var usersByUserId = mutableMapOf<Int, User>()
     private var usersByUsername = mutableMapOf<String, User>()
 
     // Autoincrement
     private var i = 0
 
     fun addUser(user: User) {
-        usersByUserID[user.userId] = user
+        usersByUserId[user.userId] = user
         usersByUsername[user.username] = user
     }
 
     fun removeUser(user: User) {
-        usersByUserID.remove(user.userId)
+        usersByUserId.remove(user.userId)
         usersByUsername.remove(user.username)
     }
 
@@ -33,22 +33,20 @@ object Users {
     }
 
     fun getUserById(id: Int): User? {
-        return usersByUserID[id]
+        return usersByUserId[id]
     }
 
 }
 
 class User(var username: String) {
 
-    var userId = getNextUserID()
+    var userId = getNextUserId()
     var ready = false
     var authToken = ""
     var color = Color.YELLOW
 
-    @Transient
-    private var passwordHash = ""
-    @Transient
-    var queue = CommandQueue()
+    @Transient private var passwordHash = ""
+    @Transient var queue = CommandQueue()
 
     constructor(username: String, password: String) : this(username) {
         updatePassword(password)
@@ -63,7 +61,6 @@ class User(var username: String) {
     }
 }
 
-// Simplification of user class to send to client
 class ClientUser(val userId: Int, var username: String, val authToken: String) {
     constructor() : this(0, "", "")
 }
