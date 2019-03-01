0fc4f3eb9e06483a4b34cffee17fea9e6465dc62
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2019-03-01 14:34:43 -0700

    rename to shardCard. Created svgs for shards.

--- client/src/app/game/gutter/gutter.component.ts
+++ client/src/app/game/gutter/gutter.component.ts
@@ -9,7 +9,7 @@ export class GutterComponent implements OnInit {
 
   constructor() { }
 
-  private trainCards = ['train1', 'train2', 'train3', 'train4', 'train1', 'train2', 'train3', 'train4'];
+  private shardCards = ['train1', 'train2', 'train3', 'train4', 'train1', 'train2', 'train3', 'train4'];
   private destCards = ['dest1', 'dest2', 'dest3', 'dest4', 'dest5', 'dest1', 'dest1', 'dest1'];
 
   ngOnInit() {
