8b2f17d8d3b497e6cf30841ff8694f3ffc7479a0
Brennon Brimhall <brennonbrimhall@gmail.com>
2019-03-01 21:18:15 -0700

    Add new server models and commands.

---
+++ server/src/models/cards.kt
@@ -0,0 +1,35 @@
+package models
+
+enum class MaterialType(val material: String) {
+    REALITY_SHARD("reality_shard"),
+    SOUL_SHARD("soul_shard"),
+    SPACE_SHARD("space_shard"),
+    MIND_SHARD("mind_shard"),
+    POWER_SHARD("power_shard"),
+    TIME_SHARD("time_shard"),
+    VIBRANIUM("vibranium"),
+    KYBER_CRYSTAL("kyber_crystal"),
+    INFINITY_GAUNTLET("infinity_gauntlet")
+}
+
+interface ICard
+
+class TrainCard(val type: MaterialType) : ICard
+class DestinationCard(val cities: Set<String>)
+
+abstract class IDeck<T>(var cards: List<T>) {
+
+    fun shuffle() {
+        cards = cards.shuffled()
+    }
+
+    fun getNext(): T {
+        var top = cards.first()
+        cards = cards.drop(1)
+
+        return top
+    }
+}
+
+class TrainCardDeck(var trainCards: List<TrainCard>) : IDeck<TrainCard>(trainCards)
+class DestinationCardDeck(var destinationCards: List<DestinationCard>) : IDeck<DestinationCard>(destinationCards)
