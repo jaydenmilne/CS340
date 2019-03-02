034a636da5ab0643845ff079506ad921e16019d2
gassajor000 <43421414+gassajor000@users.noreply.github.com>
2019-03-02 15:25:38 -0700

    Gutter component (#94)
    * Add mat-tabs to gutter.
    
    * Added horizontal scrolling. Other tweaks to gutter.
    
    * rename to shardCard. Created svgs for shards.
    
    * Fixed SVG files
    
    * Tweaks to gutter component layout. Removed margin on body container.
    
    * Added new icon for combined shards count. Removed box around card.
    
    * Fix merge conflicts.

--- client/src/app/game/game.module.ts
+++ client/src/app/game/game.module.ts
@@ -5,8 +5,7 @@ import { PlayerListComponent } from './player-list/player-list.component';
 import { GutterComponent } from './gutter/gutter.component';
 import { MapComponent } from './map/map.component';
 import { BankComponent } from './bank/bank.component';
-
-import { MatGridListModule, MatChipsModule, MatDialogModule, MatButtonModule } from '@angular/material';
+import { MatGridListModule, MatChipsModule, MatDialogModule, MatButtonModule, MatTabsModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { PlayerInfoDialogComponent } from './player-list/player-list.component'
 import { ChatModule } from '../chat/chat.module';
@@ -16,16 +15,14 @@ import { ChatModule } from '../chat/chat.module';
   declarations: [GameComponent, PlayerListComponent, GutterComponent, MapComponent, BankComponent, PlayerInfoDialogComponent],
   entryComponents: [ PlayerInfoDialogComponent ],
   imports: [
-
     CommonModule, 
     MatGridListModule,
+    MatTabsModule,
     MatCardModule,
     MatChipsModule,
     MatDialogModule,
     MatButtonModule,
-
     ChatModule
-
   ]
 })
 export class GameModule { }
--- client/src/app/game/gutter/gutter.component.ts
+++ client/src/app/game/gutter/gutter.component.ts
@@ -9,7 +9,27 @@ export class GutterComponent implements OnInit {
 
   constructor() { }
 
+  private shardCards = [
+    new shardCount('Reality Stone', 1, 'reality_stone.svg'),
+    new shardCount('Soul Stone', 2, 'soul_stone.svg'),
+    new shardCount('Time Stone', 3, 'time_stone.svg'),
+    new shardCount('Space Stone', 4, 'space_stone.svg'),
+    new shardCount('Power Stone', 5, 'power_stone.svg'),
+    new shardCount('Mind Stone', 6, 'mind_stone.svg'),
+    new shardCount('Palladium', 7, 'palladium.svg'),
+    new shardCount('Vibranium', 8, 'vibranium.svg'),
+    new shardCount('Infinity Gauntlet', 9, 'gauntlet.svg'),
+  ]
+
+
+  private destCards = ['dest1', 'dest2', 'dest3', 'dest4', 'dest5', 'dest1', 'dest1', 'dest1'];
+
   ngOnInit() {
   }
 
 }
+
+class shardCount {
+  constructor(public type: string, public count: number, public img: string){}
+}
+
