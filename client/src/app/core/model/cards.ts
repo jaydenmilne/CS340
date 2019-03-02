15e7ec1cb5a078229a0be0d3472cc0acfd262cc2
Brennon Brimhall <brennonbrimhall@gmail.com>
2019-03-02 15:44:04 -0700

    Add GamePlayer model. (#93)
    * Add GamePlayer model.
    
    * Add cards.
    
    * Definitely not uppercase.
    
    * Fix deserialization checks.
    
    * Remove shuffling on client-side.
    
    * Make cards have a better inheritance tree.

---
+++ client/src/app/core/model/cards.ts
@@ -0,0 +1,102 @@
+export enum MaterialType {
+    REALITY_SHARD = "reality_shard",
+    SOUL_SHARD = "soul_shard",
+    SPACE_SHARD = "space_shard",
+    MIND_SHARD = "mind_shard",
+    POWER_SHARD = "power_shard",
+    TIME_SHARD = "time_shard",
+    VIBRANIUM = "vibranium",
+    PALLADIUM = "palladium",
+    INFINITY_GAUNTLET = "infinity_gauntlet"
+}
+
+export enum City {
+	DARK_DIMENSION = "darkdimension",
+    GIBBORIM = "gibborim",
+    GALACTUS = "galactus",
+    ZENN_LA = "zennla",
+    KNOWHERE = "knowhere",
+    CHITAURI_SANCTUARY = "chitaurisanctuary",
+    ZEN_WHOBERI = "zenwhoberi",
+    EGO = "ego",
+    KITSON = "kitson",
+    CAVE_OF_THE_DRAGON = "caveofthedragon",
+    KUN_LUN = "kunlun",
+    VANAHEIM = "vanaheim",
+    TITAN = "titan",
+    VORMIR = "vormin",
+    SAKAAR = "sakaar",
+    XANDAR = "xandar",
+    CAVE_OF_AGES = "caveofages",
+    SURTURS_LAIR = "surturslair",
+    HALA = "hala",
+    SVARTLHEIM = "svartlheim",
+    NIFLHEIM = "niflheim",
+    CONTRAXIA = "contraxia",
+    QUANTUM_REALM = "quantumrealm",
+    ASGARD = "asgard",
+    NIDAVELLIR = "nidavellir",
+    YOTUNHEIM = "yotunheim",
+    HONG_KONG_SANCTUM = "hongkongsanctum",
+    WAKANDA = "wakanda",
+    SOKOVIA = "sokovia",
+    PYM_LABS = "pymlabs",
+    AVENGERS_HQ = "avengershq",
+    NEW_YORK_CITY = "newyorkcity",
+    TRISKELION = "triskelion",
+    HELICARRIER = "helicarrier",
+    KAMAR_TAJ = "kamartaj",
+    MUSPELHEIM = "muspelheim"
+}
+
+abstract class ICard {}
+
+export class DestinationCard extends ICard {
+	private cities: City[]
+
+	public setCities(cities: City[]) {
+		this.cities = cities;
+	}
+
+	public getCities(): City[] {
+		return this.cities;
+	}
+}
+
+export class TrainCard extends ICard {
+	private type: MaterialType
+
+	public setMaterialType(type: MaterialType) {
+		this.type = type;
+	}
+
+	public getMaterialType(): MaterialType {
+		return this.type;
+	}
+}
+
+abstract class ICardDeck<T> {
+	protected cards: T[]
+	abstract getTop(): T;
+	abstract size(): Number;
+}
+
+export class CardDeck extends ICardDeck<DestinationCard> {
+	public getTop(): DestinationCard {
+		return this.cards.shift()
+	}
+
+	public size(): Number {
+		return this.cards.length
+	}
+}
+
+export class TrainCardDeck extends ICardDeck<TrainCard> {
+	public getTop(): TrainCard {
+		return this.cards.shift()
+	}
+
+	public size(): Number {
+		return this.cards.length
+	}
+}
