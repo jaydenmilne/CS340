5482f711abfeb88b1a9cd291c1f799cca76185c2
Brennon Brimhall <brennonbrimhall@gmail.com>
2019-03-02 15:23:46 -0700

    Add cards.

---
+++ client/src/app/core/model/cards.ts
@@ -0,0 +1,126 @@
+export enum MaterialType {
+    REALITY_SHARD = "reality_shard",
+    SOUL_SHARD = "soul_shard",
+    SPACE_SHARD = "space_shard",
+    MIND_SHARD = "mind_shard",
+    POWER_SHARD = "power_shard",
+    TIME_SHARD = "time_shard",
+    VIBRANIUM = "vibranium",
+    PALLADIUM = "palladium",
+    INFINITY_GAUNTLET = "infinity_gauntlet"
+}
+
+export enum City {
+	DARK_DIMENSION = "darkdimension",
+    GIBBORIM = "gibborim",
+    GALACTUS = "galactus",
+    ZENN_LA = "zennla",
+    KNOWHERE = "knowhere",
+    CHITAURI_SANCTUARY = "chitaurisanctuary",
+    ZEN_WHOBERI = "zenwhoberi",
+    EGO = "ego",
+    KITSON = "kitson",
+    CAVE_OF_THE_DRAGON = "caveofthedragon",
+    KUN_LUN = "kunlun",
+    VANAHEIM = "vanaheim",
+    TITAN = "titan",
+    VORMIR = "vormin",
+    SAKAAR = "sakaar",
+    XANDAR = "xandar",
+    CAVE_OF_AGES = "caveofages",
+    SURTURS_LAIR = "surturslair",
+    HALA = "hala",
+    SVARTLHEIM = "svartlheim",
+    NIFLHEIM = "niflheim",
+    CONTRAXIA = "contraxia",
+    QUANTUM_REALM = "quantumrealm",
+    ASGARD = "asgard",
+    NIDAVELLIR = "nidavellir",
+    YOTUNHEIM = "yotunheim",
+    HONG_KONG_SANCTUM = "hongkongsanctum",
+    WAKANDA = "wakanda",
+    SOKOVIA = "sokovia",
+    PYM_LABS = "pymlabs",
+    AVENGERS_HQ = "avengershq",
+    NEW_YORK_CITY = "newyorkcity",
+    TRISKELION = "triskelion",
+    HELICARRIER = "helicarrier",
+    KAMAR_TAJ = "kamartaj",
+    MUSPELHEIM = "muspelheim"
+}
+
+export class DestinationCard {
+	private cities: City[]
+
+	public setCities(cities: City[]) {
+		this.cities = cities;
+	}
+
+	public getCities(): City[] {
+		return this.cities;
+	}
+}
+
+export class TrainCard {
+	private type: MaterialType
+
+	public setMaterialType(type: MaterialType) {
+		this.type = type;
+	}
+
+	public getMaterialType(): MaterialType {
+		return this.type;
+	}
+}
+
+export class DestinationCardDeck {
+	private cards: DestinationCard[]
+
+	public getTop(): DestinationCard {
+		return this.cards.shift()
+	}
+
+	public size(): Number {
+		return this.cards.length
+	}
+
+	// Fisher-Yates shuffle in TS. Adapted from https://www.frankmitchell.org/2015/01/fisher-yates/.
+	public shuffle() {
+		var i = 0;
+		var j = 0;
+		var temp = null;
+
+		for (i = this.cards.length - 1; i > 0; i -= 0) {
+			j = Math.floor(Math.random() * (i + 1))
+			temp = this.cards[i]
+			this.cards[i] = this.cards[j]
+			this.cards[j] = temp
+		}
+	}
+}
+
+export class TrainCardDeck {
+	private cards: TrainCard[]
+
+	public getTop(): TrainCard {
+		return this.cards.shift()
+	}
+
+	public size(): Number {
+		return this.cards.length
+	}
+
+	// Fisher-Yates shuffle in TS. Adapted from https://www.frankmitchell.org/2015/01/fisher-yates/.
+	public shuffle() {
+		var i = 0;
+		var j = 0;
+		var temp = null;
+
+		for (i = this.cards.length - 1; i > 0; i -= 0) {
+			j = Math.floor(Math.random() * (i + 1))
+			temp = this.cards[i]
+			this.cards[i] = this.cards[j]
+			this.cards[j] = temp
+		}
+	}
+}
