746701ac46f125025b49c7033c0a928165bc5e45
Justin Hawkins <jhawkins93@gmail.com>
2019-03-08 12:07:48 -0700

    More work on card service

--- client/src/app/game/card.service.ts
+++ client/src/app/game/card.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { ShardCard, DestinationCard } from '@core/model/cards';
 import { CommandRouterService } from '@core/command-router.service';
-import { UpdatePlayerCommand } from '@core/game-commands';
+import { UpdatePlayerCommand } from '@core/game-commands.ts';
 import { ServerProxyService } from '@core/server/server-proxy.service';
+import { RequestDestinationsCommand } from '@core/game-commands.ts'
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,9 @@ export class CardService {
     private serverProxy : ServerProxyService) {
     
     this.serverProxy.executeCommand(new RequestDestinationsCommand());
+    this.commandRouter.dealCards$.subscribe( cmd => this.onDealCards(cmd));
+    this.commandRouter.updateBank$.subscribe( cmd => this.onUpdateBank(cmd));
     this.commandRouter.updatePlayer$.subscribe( cmd => this.onUpdatePlayer(cmd));
-  }
-
-  private onUpdatePlayer(updatePlayerCmd : UpdatePlayerCommand) {
-    
   }
 
   public faceUpShardCards : ShardCard[];
@@ -27,4 +26,16 @@ export class CardService {
   public playerTrainCards : ShardCard[];
   public playerDestCards : DestinationCard[];
 
+  private onDealCards(dealCardsCmd : DealCardsCommand) {
+    // Need to set array of cards as observable (like in CommandRouter)
+  }
+
+  private onUpdateBank(updateBankCmd : UpdateBankCommand) {
+
+  }
+
+  private onUpdatePlayer(updatePlayerCmd : UpdatePlayerCommand) {
+    let gamePlayer = updatePlayerCmd.player;
+    
+  }
 }
