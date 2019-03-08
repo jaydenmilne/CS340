de9800ec8dddfa0c4f8cf72826a7ddf46ea2ad35
FramesTheName <frame.mlf@gmail.com>
2019-03-08 10:37:34 -0700

    Added Basic Bank Layout

--- client/src/app/game/bank/bank.component.ts
+++ client/src/app/game/bank/bank.component.ts
@@ -9,7 +9,20 @@ export class BankComponent implements OnInit {
 
   constructor() { }
 
+  private shardCards = [
+    new shardCount('Reality Stone', 1, 'reality_stone.svg'),
+    new shardCount('Soul Stone', 2, 'soul_stone.svg'),
+    new shardCount('Time Stone', 3, 'time_stone.svg'),
+    new shardCount('Space Stone', 4, 'space_stone.svg'),
+    new shardCount('Power Stone', 5, 'power_stone.svg'),
+  ]
+
   ngOnInit() {
   }
 
+  
+
+}
+class shardCount {
+  constructor(public type: string, public count: number, public img: string){}
 }
