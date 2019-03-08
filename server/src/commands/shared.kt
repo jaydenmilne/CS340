45de17f127670b7697bdf518bef3d48f0c9c73a6
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-03-08 14:13:37 -0700

    Make chat actually work

--- server/src/commands/shared.kt
+++ server/src/commands/shared.kt
@@ -1,26 +1,29 @@
 package commands
 
 import models.Games
+import models.Message
 import models.User
 
 /**
  * A command used to represent chat messages.  The server just acts like a dumb network hub and broadcasts to all users
  * in that game via their CommandQueues.
  */
-class PostChatCommand : INormalClientCommand, INormalServerCommand {
+class PostChatCommand : INormalServerCommand {
     override val command = "postChat"
-    private val gameId = ""
-    private val playerId = ""
-    private val playerName = ""
     private val message = ""
 
     override fun execute(user: User) {
-        val game = Games.games[gameId.toInt()]
+        val gameId = Games.getGameIdForPlayer(user)
+
+        val game = Games.games[gameId]
 
         if (game == null) {
             throw CommandException("The game you're posting a chat message to doesn't exist.")
         }
 
-        game.players.forEach { user:User -> user.queue.push(this) }
+        val messageObject = Message(user.userId, user.username, message, game.getNextMessageId())
+        val updateChatCommand = UpdateChatCommand(messageObject)
+
+        game.broadcast(updateChatCommand)
     }
 }
--- server/src/models/messages.kt
+++ server/src/models/messages.kt
@@ -2,7 +2,6 @@ package models
 
 import models.Games.games
 
-class Message(val username: String, val gameId: Int, val message: String) {
-    @Transient val userId = Users.getUserByUsername(username)!!.userId
-    val sequenceNum: Int = games[gameId]!!.getNextMessageId()
+class Message(val userId : Int, val username: String, val message: String, var sequenceNum: Int) {
+    constructor() : this(-1, "", "", -1)
 }
