3e159b855327bbeda82a43c3c043f3a241f61fb4
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2019-03-08 14:10:32 -0700

    Added handler for selecting destination cards.

--- client/src/app/game/game/game.component.ts
+++ client/src/app/game/game/game.component.ts
@@ -18,22 +18,17 @@ export class GameComponent implements OnInit {
   ngOnInit() {
     // Change the game connection to server mode
     this.serverConnection.changeState(new GameState(this.serverConnection));
-
-    const dialogRef = this.dialog.open(SelectDestinationCardsDialogComponent, {
-      width: '60%',
-      data: new SelectDestinationCardsData([
-        new DestinationCard({'cities':[City.XANDAR, City.ASGARD], 'points': 5}),
-        new DestinationCard({'cities':[City.CHITAURI_SANCTUARY, City.HONG_KONG_SANCTUM], 'points': 10}),
-        new DestinationCard({'cities':[City.GALACTUS, City.YOTUNHEIM], 'points': 15}),
-      ], 2),
-      disableClose: true
-    });
-    let selectedCards: SelectDestinationCardsResult;
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The select cards dialog was closed');
-      selectedCards = result;
-      // Call Card Service
-    });
   }
 
+  public handleNewDestinationCards(newDestCards: DestinationCard[]){
+    const dialogRef = this.dialog.open(SelectDestinationCardsDialogComponent, {
+      width: '60%',
+      data: new SelectDestinationCardsData(newDestCards, 2),    // TODO, this min required needs to be an argument
+      disableClose: true
+    });
+    dialogRef.afterClosed().subscribe(selectDestCardsResult => {
+      console.log('The select cards dialog was closed');
+      // TODO: Call card service
+    });
+  }
 }
