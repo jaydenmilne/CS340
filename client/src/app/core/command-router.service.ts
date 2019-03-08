1a56ba98b04bcb36fd3cb3a245c26833656052b9
jhawk93 <44040311+jhawk93@users.noreply.github.com>
2019-03-08 13:25:52 -0700

    Add card service (#107)
    * Fix unhandled exceptions with no auth token
    
    * added return statements for no auth token
    
    * More start-game logic
    
    * Template of card service
    
    * More work on card service
    
    * fixed typo

--- client/src/app/core/command-router.service.ts
+++ client/src/app/core/command-router.service.ts
@@ -26,6 +26,8 @@ export class CommandRouterService {
   public refreshGameList$ = new Subject<RefreshGameListCommand>();
   public updatePlayer$    = new Subject<UpdatePlayerCommand>();
   public changeTurn$      = new Subject<ChangeTurnCommand>();
+  public dealCards$       = new Subject<DealCardsCommand>();
+  public updateBank$      = new Subject<UpdateBankCommand>();
 
   /**
    * Identifies each command, deserializes it, and signals the observables.
@@ -49,6 +51,12 @@ export class CommandRouterService {
           break;
         case 'updatePlayer':
           this.updatePlayer$.next(new UpdatePlayerCommand(cmd));
+          break;
+        case 'dealCards':
+          this.dealCards$.next(new DealCardsCommand(cmd));
+          break;
+        case 'updateBank':
+          this.updateBank$.next(new UpdateBankCommand(cmd));
           break;
         case 'changeTurn':
           this.changeTurn$.next(new ChangeTurnCommand(cmd));
