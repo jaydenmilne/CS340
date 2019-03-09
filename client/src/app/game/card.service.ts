4d06f2e277d08b862902eeee3fa9223aef01451f
gassajor000 <43421414+gassajor000@users.noreply.github.com>
2019-03-09 15:16:25 -0700

    Select dest cards component (#112)
    * Created Dest Card module. Layout for destcard. Added city selection.
    
    * Fixed sizing, added badge with points.
    
    * Fix height and width scaling on dest cards.
    
    * Added checkmark for showing if a destination card is selected.
    
    * Added points to Destination Card model.
    
    * Created Select Destination Cards Dialog Component.
    
    * Layout and styling.
    
    * Added classes for passing data in and out of the dialog.
    
    * Removed focus directive from button that is disabled by default.
    
    * Added handler for selecting destination cards.
    
    * Fixes to get Dest Card Seleciton working. Changed player to gamePlayer.
    
    * Moved request for dest cards to ngInit of game component.
    
    * Added print friendly name for cities.

--- client/src/app/game/card.service.ts
+++ client/src/app/game/card.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ShardCard, DestinationCard } from '@core/model/cards';
 import { CommandRouterService } from '@core/command-router.service';
-import { UpdatePlayerCommand, DealCardsCommand, UpdateBankCommand, DiscardDestinationsCommand } from '@core/game-commands.ts';
+import { DealCardsCommand, UpdateBankCommand, DiscardDestinationsCommand } from '@core/game-commands.ts';
 import { ServerProxyService } from '@core/server/server-proxy.service';
 import { RequestDestinationsCommand } from '@core/game-commands.ts'
 import { Subject } from 'rxjs';
+import { SelectDestinationCardsResult, SelectDestinationCardsData } from './select-destination-cards-dialog/select-destination-cards-dialog.component';
+import { City } from '@core/model/route';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,6 @@ export class CardService {
   constructor(private commandRouter : CommandRouterService,
     private serverProxy : ServerProxyService) {
     
-    this.serverProxy.executeCommand(new RequestDestinationsCommand());
     this.commandRouter.dealCards$.subscribe( cmd => this.onDealCards(cmd));
     this.commandRouter.updateBank$.subscribe( cmd => this.onUpdateBank(cmd));
   }
@@ -23,14 +24,14 @@ export class CardService {
   public shardCardDeckSize : number;
   public shardCardDiscardSize : number;
   public destCardDeckSize : number;
-  public playerTrainCards : ShardCard[];
-  public playerDestCards : DestinationCard[];
+  public playerTrainCards : ShardCard[] = [];
+  public playerDestCards : DestinationCard[] = [];
 
-  public stagedDestinationCards$ = new Subject<DealCardsCommand>();
+  public stagedDestinationCards$ = new Subject<SelectDestinationCardsData>();
 
   private onDealCards(dealCardsCmd : DealCardsCommand) {
     if (dealCardsCmd.destinations.length > 0) {
-      this.stagedDestinationCards$.next(dealCardsCmd);
+      this.stagedDestinationCards$.next(new SelectDestinationCardsData(dealCardsCmd.destinations, dealCardsCmd.minDestinations));
     }
     this.playerTrainCards = this.playerTrainCards.concat(dealCardsCmd.shardCards);
   }
@@ -42,10 +43,13 @@ export class CardService {
     this.destCardDeckSize = updateBankCmd.destinationPileSize;
   }
 
-  public selectDestinationCards(selectedCards : DestinationCard[], discardedCards : DestinationCard[]) {
-    let outgoingCommand = new DiscardDestinationsCommand(discardedCards);
-    this.serverProxy.executeCommand(outgoingCommand);
+  public selectDestinationCards(selectCardsResult: SelectDestinationCardsResult) {
+    this.serverProxy.executeCommand( new DiscardDestinationsCommand(selectCardsResult.discardedCards));
 
-    this.playerDestCards = this.playerDestCards.concat(selectedCards);
+    this.playerDestCards = this.playerDestCards.concat(selectCardsResult.selectedCards);
+  }
+
+  public requestDestinationCards(){
+    this.serverProxy.executeCommand(new RequestDestinationsCommand());
   }
 }
---
+++ client/src/app/game/select-destination-cards-dialog/select-destination-cards-dialog.component.ts
@@ -0,0 +1,51 @@
+import { Component,  Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { DestinationCard } from '@core/model/cards';
+
+@Component({
+  selector: 'app-select-destination-cards-dialog',
+  templateUrl: './select-destination-cards-dialog.component.html',
+  styleUrls: ['./select-destination-cards-dialog.component.scss']
+})
+export class SelectDestinationCardsDialogComponent {
+  private cards: {'card': DestinationCard, 'selected': boolean}[] = [];
+  private numSelectedCards: number = 0;
+
+  constructor(
+    public dialogRef: MatDialogRef<SelectDestinationCardsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: SelectDestinationCardsData) {
+      data.newCards.forEach(card => {
+        this.cards.push({'card': card, 'selected': false});
+      });
+    }
+
+  onSelectClick(): void {
+    // Call card service to discard cards
+    let result = new SelectDestinationCardsResult([], []);
+
+    this.cards.forEach(cardPair => {
+      if (cardPair.selected){
+        result.selectedCards.push(cardPair.card);
+      }
+      else{
+        result.discardedCards.push(cardPair.card);
+      }
+    })
+
+    this.dialogRef.close(result);
+  }
+
+  onCardClick(card: {'card': DestinationCard, 'selected': boolean}){
+    card.selected = !card.selected;
+    this.numSelectedCards = this.cards.filter(destCard => destCard.selected == true).length;
+  }
+
+}
+
+export class SelectDestinationCardsData {
+  constructor(public newCards: DestinationCard[], public minRequired: number){}
+}
+
+export class SelectDestinationCardsResult {
+  constructor(public selectedCards: DestinationCard[], public discardedCards: DestinationCard[]){}
+}
