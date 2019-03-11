2bef98053f3d2735a6498a1b1883d3b770db58df
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-03-11 12:27:59 -0600

    Design by Contract (#125)

--- client/src/app/core/server/server-poller.service.ts
+++ client/src/app/core/server/server-poller.service.ts
@@ -3,6 +3,10 @@ import { interval, Subscription } from 'rxjs';
 import { ServerConnectionService } from './server-connection.service';
 import { ServerPollerState, NormalPollingState, ManualPollingState } from './server-poller-state';
 
+/**
+ * ServerPollingService
+ * Periodically polls the server, behaves differently when disconnected, can be run manually.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -11,11 +15,17 @@ export class ServerPollerService {
   public DISCONNECTED_POLLING_INTERVAL_MS = 5000;
 
   private state: ServerPollerState;
+  // Public so that the state classes can have access
   public manualPolling = false;
   // Timer that triggers polls
   private pollingTrigger$ = interval(this.POLLING_INTERVAL_MS);
   private sub: Subscription;
-
+  
+  /**
+   * Preconditions: ServerConnectionService running
+   * Postcontitions: State will be "NormalPollingState"
+   * @param connection 
+   */
   constructor(public connection: ServerConnectionService) {
 
     this.setState(new NormalPollingState());
@@ -23,6 +33,11 @@ export class ServerPollerService {
 
   }
 
+  /**
+   * Preconditions: None
+   * Postconditions: State will be newState, old state will be properly exited
+   * @param newState 
+   */
   public setState(newState) {
     if (this.state) {
       this.state.exit(this);
@@ -32,6 +47,13 @@ export class ServerPollerService {
     this.state.enter(this);
   }
 
+  /**
+   * Preconditions: None
+   * Postconditions: this.sub will be the new subscription to the timer
+   *                 this.pollingTrigger$ will the new interval timer
+   *                 this.poll() will be called when this.pollingTrigger$ emits event
+   * @param newInterval 
+   */
   public changeInterval(newInterval: number) {
     if (this.sub) {
       this.sub.unsubscribe();
@@ -48,6 +70,10 @@ export class ServerPollerService {
 
   /**
    * Increases polling interval, subscribes to listen for a successful tranmission
+   * Preconditions: In a defined state
+   * Postconditions: Normal Polling -> Error Polling
+   *                 Error Polling -> no state change
+   *                 Manual Polling -> no state change
    */
   private onServerDisconnected() {
     this.state.onDisconnect(this);
@@ -55,11 +81,21 @@ export class ServerPollerService {
 
   /**
    * Does what it says on the tin
+   * Preconditions: In a defined state
+   * Postconditions: In same state
    */
   public poll() {
     this.connection.poll();
   }
 
+  /**
+   * Listener to disable shortcut keys
+   * Preconditions: None
+   * Postconditions: if event.key is m and manually polling, NormalPollingState
+   *                 if event.key is m and automatically polling, ManualPollingState
+   *                 if event.key is p, a poll will be sent.
+   * @param event 
+   */
   public handleKeypress(event : KeyboardEvent) {
     if (event.key == "m") {
       if (this.manualPolling) {
