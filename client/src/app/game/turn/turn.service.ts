91e1275bfa682cf739be7fa523c3a143fa38e57a
Brennon Brimhall <brennonbrimhall@gmail.com>
2019-03-23 19:51:22 -0400

    Finish turn service. (#136)
    * Finish turn service.

--- client/src/app/game/turn/turn.service.ts
+++ client/src/app/game/turn/turn.service.ts
@@ -1,16 +1,69 @@
 import { Injectable } from '@angular/core';
+import { ITurnState, NotPlayersTurnState } from './turn-states';
+import { PlayerService } from '../player.service';
+import { ChangeTurnCommand } from '@core/game-commands';
+import { CommandRouterService } from '@core/command-router.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TurnService {
-  constructor() { }
+  private state: ITurnState;
+  private router: CommandRouterService;
 
-  isMyTurn(): boolean { return true; }
-  canDrawShards(): boolean { return true; }
-  canDrawWild(): boolean { return true; }
-  canDrawDestinations(): boolean { return true; }
-  canClaimRoutes(): boolean { return true; }
-  setNextState(ITurnState) { }
+  constructor(playerService: PlayerService, router: CommandRouterService) {
+    this.state = new NotPlayersTurnState(playerService, this);
+    this.router = router;
+
+    this.router.changeTurn$.subscribe(cmd => this.onChangeTurn(cmd));
+  }
+
+  isMyTurn(): boolean {
+    return this.state.isMyTurn();
+  }
+
+  canDrawShards(): boolean {
+    return this.state.canDrawShards();
+  }
+
+  canDrawWild(): boolean {
+    return this.state.canDrawWild();
+  }
+
+  canDrawDestinations(): boolean {
+    return this.state.canDrawDestinations();
+  }
+
+  canClaimRoutes(): boolean {
+    return this.state.canClaimRoutes();
+  }
+
+  setNextState(state: ITurnState) {
+    this.state = state;
+  }
+
+  onChangeTurn(cmd: ChangeTurnCommand) {
+    return this.state.onChangeTurn(cmd);
+  };
+
+  onClaimRoute() {
+    return this.state.onClaimRoute();
+  };
+
+  onDrawDeckShardCard() {
+    return this.state.onDrawDeckShardCard();
+  };
+
+  onDrawDestCard() {
+    return this.state.onDrawDestCard();
+  };
+
+  onDrawFaceUpShardCard() {
+    return this.state.onDrawFaceUpShardCard();
+  };
+
+  onDrawFaceUpWildCard() {
+    return this.state.onClaimRoute();
+  };
 
 }
