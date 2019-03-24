572e38e8d66bf953e2353e18df5434af778cf3ec
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2019-03-23 21:11:44 -0600

    bug fixes for route claiming.

--- server/src/models/cards.kt
+++ server/src/models/cards.kt
@@ -18,6 +18,22 @@ interface ICard
 
 class ShardCard(val type: MaterialType) : ICard{
     constructor() : this(MaterialType.INFINITY_GAUNTLET)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ShardCard
+
+        if (type != other.type) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return type.hashCode()
+    }
+
 }
 
 class DestinationCard(val cities: Set<String>, val points: Int) {
