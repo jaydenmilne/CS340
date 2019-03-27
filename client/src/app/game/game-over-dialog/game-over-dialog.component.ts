fedb52193bfd7638989855379f60565b6b54dd43
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2019-03-26 18:16:31 -0600

    fill out game over dialog.

--- client/src/app/game/game-over-dialog/game-over-dialog.component.ts
+++ client/src/app/game/game-over-dialog/game-over-dialog.component.ts
@@ -8,10 +8,13 @@ import { PlayerPoint } from '@core/model/player-point';
   styleUrls: ['./game-over-dialog.component.scss']
 })
 export class GameOverDialogComponent implements OnInit {
-
+  dataSource: PlayerPoint[];
+  displayedColumns: string[] = ['player', 'routes', 'destinations', 'penalties', 'longestRoute', 'total'];
   constructor(
     public dialogRef: MatDialogRef<GameOverDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: GameOverViewData) {
+      this.dataSource = data.pointTotals;
+      this.dataSource.sort((a, b) => b.totalPoints - a.totalPoints);
     }
 
   ngOnInit() {
