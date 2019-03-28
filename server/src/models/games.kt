07fe76be464be6c38ee6f22204d15b729af14772
MARAUDERSMAP\gassa <lgassjsg@gmail.com>
2019-03-28 15:10:34 -0600

    Changed getGameForPlayer to be consistant.

--- server/src/models/games.kt
+++ server/src/models/games.kt
@@ -34,7 +34,7 @@ object Games {
     }
 
     fun getGameForPlayer(user: User): Game? {
-        return games.filter { p -> p.value.players.contains(user) }.values.toMutableList()[0]
+        return games.filter { p -> p.value.players.contains(user) }.values.firstOrNull()
     }
 
     fun getGameIdForPlayer(user: User): Int? {
