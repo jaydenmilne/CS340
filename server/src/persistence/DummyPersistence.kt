a9d58d165678b8f9a1deebc6ce512f15483e9422
Brennon Brimhall <brennonbrimhall@gmail.com>
2019-04-15 16:13:10 -0600

    Fix interfaces. (#251)

--- server/src/persistence/DummyPersistence.kt
+++ server/src/persistence/DummyPersistence.kt
@@ -8,38 +8,40 @@ import ICommandDAO
 import IUserDAO
 import IGameDAO
 
-class DummyPersistenceManager: IPersistenceManager {
+class DummyPersistenceManager(persistenceManager: IPersistenceManager): IPersistenceManager {
     override fun openTransaction() {}
 
     override fun closeTransaction(commit: Boolean) {}
 
-    override fun getCommandDAO(): ICommandDAO { return DummyCommandDAO()}
+    override fun getCommandDAO(): ICommandDAO { return DummyCommandDAO(this)}
 
-    override fun getUserDAO(): IUserDAO { return DummyUserDAO() }
+    override fun getUserDAO(): IUserDAO { return DummyUserDAO(this) }
 
-    override fun getGameDAO(): IGameDAO { return DummyGameDAO() }
+    override fun getGameDAO(): IGameDAO { return DummyGameDAO(this) }
 
     override fun initialize(): Boolean { return true }
 
     override fun clear(): Boolean { return true }
 }
 
-class DummyCommandDAO: ICommandDAO {
+class DummyCommandDAO(persistenceManager: IPersistenceManager): ICommandDAO(persistenceManager) {
     override fun persistCommand(command: ICommand, gameID: Int) {}
 
     override fun loadCommands(): List<ICommand> {
         return listOf()
     }
+
+    override fun clearCommandsForGame(gameID: Int) {}
 }
 
-class DummyUserDAO: IUserDAO {
+class DummyUserDAO(persistenceManager: IPersistenceManager): IUserDAO(persistenceManager) {
     override fun persistUser(user: IUser) {}
 
     override fun loadUsers(): List<IUser> {
         return listOf()
     }
 }
-class DummyGameDAO: IGameDAO {
+class DummyGameDAO(persistenceManager: IPersistenceManager): IGameDAO(persistenceManager) {
     override fun persistGame(game: IGame) {}
 
     override fun loadGames(): List<IGame> {
